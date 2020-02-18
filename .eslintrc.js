84d4f8caa19a19049044d5eab389bd12d4c084e0
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2020-02-18 17:12:02 +0100

    updates van 2019

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "warn",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [
+            "warn"
+        ],
+        "no-console": [
+            "warn"
+        ]
+
+    },
+    "globals": {
+        "AFRAME": false
+    }
+};
